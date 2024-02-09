a39d07dcf83d3ad5aea653ad0a56ba58f3a681ed
Chakka Phani Simha <chakkaphanisimha@gmail.com>
2024-02-09 18:39:04 +0530

    adding day9.js file

---
+++ day9.js
@@ -0,0 +1,19 @@
+// Which of the following are examples of node modules?
+
+// a. Express
+
+// b. Body-parser
+
+// c. Socket.io
+
+// d. All of the above
+
+//Ans - All of the above (D)
+//all the node modules are installed into the project via npm. below described modules are also being installed into the project via npm
+// a. Express - a web application framework for Node.js.
+
+// b. Body-parser - a middleware module for handling JSON, Raw, Text, and URL-encoded form data.
+
+// c. Socket.io - a library that enables real-time, bidirectional, and event-based communication between web clients and servers.
+
+// So, the correct answer is: d. All of the above.
